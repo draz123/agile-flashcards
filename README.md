3027e85aad493bddd740347df4dc85f45222b882
draz123 <albert.podraza@gmail.com>
2017-05-27 22:30:17 +0200

    Gradle support added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Agile--fleshcards
+
+Status: In progress
+
+## About
+Project designed in react-native framework. It's connection of translator and flashcards. We can create language fleshcards, make quizzes and our personal dictionaries.
---
+++ client/AppStyles.js
@@ -0,0 +1,26 @@
+/**
+ * Created by SG0222865 on 4/11/2017.
+ */
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+		backgroundColor: 'black',
+        justifyContent:'center'
+    },
+    titleText: {
+        justifyContent:'space-around',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    wordDescription:{
+        fontSize:20,
+        fontWeight: 'bold',
+    },
+    listView:{
+        fontSize: 24,
+    }
+
+});
